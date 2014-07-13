0eac42f557bbda231c0b1f05c24937a698bcb2b1
Aquagrunty <aquagrunty@gmail.com>
2014-07-12 14:45:56 -1000

    Added some descriptions of the intended behavior of this application

--- webpgp.rb
+++ webpgp.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "datamapper"
+require "digest/sha2"
 
 # Requirements
 # -ruby 
@@ -20,44 +21,12 @@ get "/:user_id" do
     # restore the encrypted private key (and message fields)
 end
 
+get "/hash" do
+    h = Digest::SHA2.new << File.open('views/index.erb').read
+    erb h.base64digest
+end
 
-__END__
-
-@@index
-<script src="js/openpgp.min.js"></script>
-<form id="encrypt">
-    <div class="row">
-        <div class="form-group col-md-6">
-            <label>Public Key</label>
-            <textarea placeholder="Enter PGP private key here..." id="public-key" class="form-control"></textarea>
-        </div>
-        <div class="form-group col-md-6">
-            <label>Message</label>
-            <textarea id="message" class="form-control">(address goes here)</textarea>
-        </div>
-    </div>
-    <div class="form-group">
-        <input type="submit" class="btn btn-success btn-lg btn-block" id="encrypt" value="Encrypt" />
-        <input type="submit" class="btn btn-success btn-lg btn-block" id="encrypt" value="Decrypt" />
-        
-    </div>
-    <div class="row">
-        <div class="form-group col-md-4 text-center">
-             <label>Private Key</label>
-            <textarea placeholder="Enter PGP private key here..." id="public-key" class="form-control"></textarea>
-        </div>
-        <div class="form-group col-md-8">
-            <label>Result</label>
-            <textarea id="result" class="form-control"></textarea>
-        </div>
-</form>
-<script>
-    $('#encrypt').submit(function(e) {
-        e.preventDefault();
-        public_key = $('#public-key').val();
-        message = $('#message').val();
-        pubkey = openpgp.key.readArmored(public_key).keys[0];
-        result = openpgp.encryptMessage([pubkey], message);
-        $('#result').val(result);
-    });
-</script>
+get "/openpgp" do
+    h = Digest::SHA2.new << File.open('openpgp.js').read
+    erb h.base64digest
+end
