3eee843bf4bf9ce26a71dbacb89e713a352f96de
Cody Moniz <cody@uievolution.com>
2014-07-16 09:40:36 -1000

    Added some hash stuff and made the hashes work

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ webpgp
 ======
 
 The server code for an online PGP encryption/decryption service. Private keys are stored in the database encrypted with a symmetric key, which when fetched by the webpage is decrypted using javascript. 2 Factor Auth enabled. Users can choose to uses encrypted data stored in localstorage, or stored on the server, or both.
+
+## Hash Verification
+Most recent openpgp.js SHA256/base64 hash: poR2nF9fnBXUWRUyT0J1BV4z/64wjPlltLKyzaw1qcs=
+Most recent webpgp.rb SHA256/base64 hash: IYMdIRjPhSSQgUrlDyzcvMuKjmZ32sDfFNPc6LqdhYQ=
--- webpgp.rb
+++ webpgp.rb
@@ -1,5 +1,5 @@
 require "sinatra"
-require "datamapper"
+require "data_mapper"
 require "digest/sha2"
 
 # Requirements
@@ -13,20 +13,20 @@ get "/" do
     erb :index
 end
 
+get "/hash" do
+    h = Digest::SHA2.new << File.open('views/index.erb').read
+    h.base64digest
+end
+
+get "/openpgp" do
+    h = Digest::SHA2.new << File.open('openpgp.js').read
+    h.base64digest
+end
+
 post "/save" do
     # save the encrypted private key (and message fields)
 end
 
 get "/:user_id" do
     # restore the encrypted private key (and message fields)
-end
-
-get "/hash" do
-    h = Digest::SHA2.new << File.open('views/index.erb').read
-    erb h.base64digest
-end
-
-get "/openpgp" do
-    h = Digest::SHA2.new << File.open('openpgp.js').read
-    erb h.base64digest
 end
